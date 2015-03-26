e61753855a05c157b501b7afc3b1f14405363733
Dublx <luis.faustino@gmail.com>
2015-03-26 20:02:01 +0000

    rm nsq/data

--- shell-utils/start-nsq.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -euo pipefail
-
-LOG=../nsq/log
-DATA=../nsq/data
-
-nsqlookupd &> $LOG/nsqlookupd.log &
-nsqd -data-path="$DATA" --lookupd-tcp-address=127.0.0.1:4160 &> $LOG/nsqd.log &
-nsqadmin --lookupd-http-address=127.0.0.1:4161 &> $LOG/nsqadmin.log &
